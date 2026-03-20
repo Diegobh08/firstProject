8045731f308ab1267952b905dff8b17c2e364483
Diego Barrios <diego.barrios@unisimon.edu.co>
2026-03-20 10:06:19 -0500

    add first project

--- main.js
+++
@@ -1,8 +0,0 @@
-﻿// Evento simple solicitado
-const cta = document.getElementById("cta");
-if (cta) {
-  cta.addEventListener("click", () => {
-    // No realiza ninguna accion adicional por requisito
-    console.log("CTA presionado");
-  });
-}
